55fab886f6df785f3a76c6e63c526298296f0335
Solomatin Dmitry <chlok@mail.ru>
2022-03-19 21:47:58 +0300

    initial commit

--- src/main/java/ru/liga/App.java
+++ src/main/java/ru/liga/App.java
@@ -5,20 +5,21 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.liga.view.Bot;
 
+import java.util.Map;
+
 
 /**
  *
  */
 public class App {
-    static String BOT_NAME = "CurrencyTelegramBot";
-    static String BOT_TOKEN = "5201090499:AAGnczO3fuwdARl9L76rruLZndB-dNG9eZo";
+    private static final Map<String, String> getenv = System.getenv();
 
 
     public static void main(String[] args) {
 
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN));
+            botsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
